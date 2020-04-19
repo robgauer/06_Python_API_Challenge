1a9a98637b5646d30ad82fe8f38d031200c56419
Rob Gauer <gauerr@gmail.com>
2020-04-19 04:30:14 -0500

    Python-API-Challenge-Homework-WeatherPy-RobGauer-2

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "7353f39050ebd58b7f2079c24baa85f5"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAtGIaEU0IXi5RuC9hmdjTftZhikNMFNiI"
+g_key = ""
